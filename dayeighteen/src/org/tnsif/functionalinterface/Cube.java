c8bebcdbd30122d3b63fc25419e0834a42641bd3
KARUNA <KARUNA@DESKTOP-CILKO93>
2023-10-23 18:53:47 +0530

    first commit

---
+++ dayeighteen/src/org/tnsif/functionalinterface/Cube.java
@@ -0,0 +1,10 @@
+package org.tnsif.functionalinterface;
+
+//Program to define functional interface 
+
+
+@FunctionalInterface
+public interface Cube {
+	int getCube(int no);
+	//int getSquare(int no); //CTE 
+}
---
+++ dayeighteen/src/org/tnsif/functionalinterface/Operations.java
@@ -0,0 +1,9 @@
+package org.tnsif.functionalinterface;
+
+//Program to define functional interface 
+
+
+@FunctionalInterface
+public interface Operations {
+	float performArithmetic(int a, int b);
+}
